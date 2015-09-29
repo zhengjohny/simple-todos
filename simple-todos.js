fefb8edef1c058f4b574ed6b7e497221593315eb
Johny <johnyz@hawaii.edu>
2015-09-29 11:30:44 -1000

    Tutorial
    Copy and pasting

--- simple-todos.js
+++ simple-todos.js
@@ -1,23 +1,29 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+    }
   });
 }
